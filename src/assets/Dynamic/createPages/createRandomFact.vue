acebd75536e7208a5ae665054816087340137d12
Tayyab Javed <tayyabdev@outlook.com>
2018-05-31 15:30:21 +0100

    update

--- src/assets/Dynamic/createPages/createRandomFact.vue
+++ src/assets/Dynamic/createPages/createRandomFact.vue
@@ -30,37 +30,68 @@
     color:grey;
 }
  
+ .progressBarCont{
+     display: flex;
+     align-items: center;
+     justify-content: center;
+ }
 </style>
 
 
 <template>
+<div>
     <div class="createFactContainer">
-        create a fact here, fill in the form below
+          create a fact here, fill in the form below
         <input v-model="factTyped"  v-on:keyup.enter="submitFact" v-on:keyup="typingFact" class="factInput" type="text" placeholder="type a fact and push enter"/>
         <div class="charRemaining">{{charRemaining}}/141 characters</div>
+        
+     </div>
+     <div v-if="isLoading" class="progressBarCont">
+         <v-progress-circular :size="60" :width="6" indeterminate color="red"></v-progress-circular>
+     </div>
      </div>
 </template>
 
 
 <script>
+import {db} from './../../firestore.js';
+
+console.log(db);
+
+//import the firebase database instance
 export default{
     data:function(){
         return {
             charRemaining:0,
-            factTyped:''
+            factTyped:'',
+            isLoading:false
         }
-    },methods:{
+    }, 
+    firestore(){
+            return {
+                randiFactsCollection:db.collection("randiFactsCollection")
+            }
+        },methods:{
         typingFact(){
             var totalChars= this.factTyped.length;
             this.charRemaining=totalChars;
         },submitFact(){
+            var home=this;
             if(this.factTyped.length>=142){
                 alert("too many characters delete some chracters and try submitting again");
+
             }else{
-                alert("fact submitted");
-                this.factTyped="";
-                //crear fact input
-            }
+                 //crear fact input
+                 home.isLoading=true;
+                 this.$firestore.randiFactsCollection.add({
+                    fact:home.factTyped
+                }).then((result) => {
+                    home.isLoading=false;
+                    this.factTyped="";
+                }).catch((err) => {
+                    
+                });
+             }
         }
     }
 }
