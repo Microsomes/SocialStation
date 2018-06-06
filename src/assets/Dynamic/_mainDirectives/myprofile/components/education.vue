8dec46ada180e833a532345376be2f82e5fbe10c
microsomes <tayyabdev@outlook.com>
2018-06-06 21:23:17 +0100

    init education view page

--- src/assets/Dynamic/_mainDirectives/myprofile/components/education.vue
+++ src/assets/Dynamic/_mainDirectives/myprofile/components/education.vue
@@ -1,10 +1,30 @@
 
 <style scoped>
-
+    .breifInfoContainer{
+        border:1px solid black;
+        min-height: 1000px;
+    }
+    .breifInfoContainer{
+        display: flex;
+        font-family: 'Roboto', sans-serif;
+    }
+    .educationColumnLeft{
+        border:1px solid black;
+        min-width: 700px;
+    }
+    .educationColumnRight{
+        border:1px solid black;
+        width: 600px;
+    }
 </style>
 <template>
 <div class="breifInfoContainer">
-    education
+    <div class="educationColumnLeft">
+        education and experience
+    </div>
+    <div class="educationColumnRight">
+        social interactions who else is in education etc social widgets
+    </div>
 </div>
 </template>
 
