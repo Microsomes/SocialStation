466cac1771d0586a570ff3dd1f6b3ec816b61966
microsomes <tayyabdev@outlook.com>
2018-06-13 05:03:31 +0100

    users can now login with username instead of a password

--- src/assets/Dynamic/_mainDirectives/entryComponents/loginForm.vue
+++ src/assets/Dynamic/_mainDirectives/entryComponents/loginForm.vue
@@ -49,9 +49,10 @@ h1{
             Social station is an open source modular social media platform. Learn more at <a target="_blank" style="color:white" href="https://socialstation.info">socialstation.info</a>
          </div>
         <div class="signUpContainer">
-            <input v-on:keyup.enter="authenticateUser" v-model="formValues.email" type="text" placeholder="email"/>
+            <input v-on:keyup.enter="authenticateUser" v-model="formValues.username" type="text" placeholder="username"/>
             <input v-on:keyup.enter="authenticateUser"  v-model="formValues.password" type="password" placeholder="password"/>
             <v-btn @click="authenticateUser()" style="background:white;margin-top:20px;font-family: 'Roboto', sans-serif;padding:0px;">Login</v-btn>
+            <em style="text-align:center;" v-if="feedback">{{feedback}}</em>
         </div>
     </div>
 </template>
@@ -59,6 +60,7 @@ h1{
 <script>
 import {auth} from './../../../firestore.js';
 //import auth object
+import {db} from './../../../firestore.js';
 
 //these component should de decoupled but fuck it im running on a short time frame so for now it wont be
 //todo decouple auth logic from this component to the parent
@@ -66,8 +68,9 @@ import randomFactsViewer from './../../codeModules/randomFact';
 export default{
 data:function(){
     return {
+        feedback:null,
         formValues:{
-            email:'',
+            username:'',
             password:''
         }
     }
@@ -75,28 +78,43 @@ data:function(){
     randomFactsViewer,
 },methods:{
     authenticateUser(){
-        //send a politly message to firebase to ask them if user account exists
-        //and to nicely ask for a unique id back 
-        //TODO check if username and password fields are filleds
-        if(this.formValues.email!="" && this.formValues.password!=""){
-            //username and password fields are filled
-        
-            auth.signInWithEmailAndPassword(this.formValues.email,this.formValues.password).catch(function(err){
-                //handle erros here
-                var errorCode= err.code;
-                var errorMessage= err.message;
-                if(err){
-                    alert(errorMessage);
+    
+        //check if username and password are first entered
+        if(this.formValues.username && this.formValues.password){
+            //password and username are entered
+            
+            this.feedback="Connecting to db";
+            var ref= db.collection("users").where("username","==",this.formValues.username);
+            ref.get().then(doc=>{
+                if(doc.empty){
+                    //username does not exist
+                    this.feedback="username does not exist";
+                }else{
+                    //username exists
+                   doc.forEach(user=>{
+                    var userEmail= user.data().email;
+                    //grabbed user email time to sign him/her in now
+                    auth.signInWithEmailAndPassword(userEmail,this.formValues.password).then(auth=>{
+                        //signed in
+                    }).catch(err=>{
+                        this.feedback=err.message;
+                    })
+                })
                 }
-            });
-            console.log("logging in");
-            this.$router.push("/dashboard");
+                 
+            }).catch(err=>{
+                this.feedback=err;
+                console.log(err);
+            })
 
-            return;
+
+        }else{
+            this.feedback="Please enter your Username and Password";
         }
 
-        //TODO send toast to user to enter all fields
-        alert("fill in all fields ");
+        //connect to firestore and grabs users email address by password
+
+        //sign user in with email address and password
 
     }
 }
